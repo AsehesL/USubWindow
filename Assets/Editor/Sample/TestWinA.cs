ef42980418c77283cacbdaf031830de084fbc29a
gitsen <342922630@qq.com>
2017-09-10 20:42:09 +0800

    提交示例代码

---
+++ Assets/Editor/Sample/TestWinA.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TestWinA : MDIEditorWindow {
+
+    [MenuItem("Test/TestWinA")]
+    static void Init()
+    {
+        TestWinA win = TestWinA.CreateWindow<TestWinA>();
+    }
+
+    [SubWindow("SunWinA", SubWindowIcon.Game)]
+    private void SubWinA(Rect main)
+    {
+        GUI.Label(new Rect(main.x, main.y, main.width, 20), "SubWinA");
+    }
+
+    [SubWindow("SunWinB", SubWindowIcon.Project)]
+    private void SubWinB(Rect main)
+    {
+        GUI.Label(new Rect(main.x, main.y, main.width, 20), "SubWinB");
+    }
+
+    [SubWindow("SunWinC", SubWindowIcon.Search)]
+    private void SubWinC(Rect main)
+    {
+        GUI.Label(new Rect(main.x, main.y, main.width, 20), "SubWinC");
+    }
+
+    [SubWindow("SunWinD", SubWindowIcon.None)]
+    private void SubWinD(Rect main)
+    {
+        GUI.Label(new Rect(main.x, main.y, main.width, 20), "SubWinD");
+    }
+}
